e5bbc6c6fff5267e8de26319bef9c11d40f59c6b
tragiccode <michael@tragiccode.com>
2017-04-11 18:35:14 -0500

    asd

--- site/profile/manifests/splunk/forwarder.pp
+++ site/profile/manifests/splunk/forwarder.pp
@@ -3,7 +3,7 @@
 class profile::splunk::forwarder {
 
   class { '::splunk::params':
-    src_root => 'https://s3.amazonaws.com/mfyffe',
+    src_root => 'https://s3.amazonaws.com\\mfyffe',
     version  => '6.5.3',
     build    => '36937ad027d4',
   }
