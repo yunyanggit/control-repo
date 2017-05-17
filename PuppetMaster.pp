69be25274c35ad8fad4c15eff3f8c786a17d8171
tragiccode <michael@tragiccode.com>
2017-05-17 14:25:28 -0500

    Lets keep it simple

--- PuppetMaster.pp
+++ PuppetMaster.pp
@@ -1,22 +1,17 @@
-class puppetmaster {
-
-  node_group { 'PE Master - Customized':
-    ensure               => 'present',
-    rule                 => ['and', ['~', ['fact', 'fqdn'], $facts['fqdn']]],
-    classes              => {
-      'puppet_enterprise::profile::master' => {
-        'r10k_remote'                 => 'https://github.com/TraGicCode/control-repo.git',
-        'code_manager_auto_configure' => true,
-      },
-      'pe_repo::platform::windows_x86_64' => {
-
-      },
+node_group { 'PE Master - Customized':
+  ensure               => 'present',
+  rule                 => ['and', ['~', ['fact', 'fqdn'], $facts['fqdn']]],
+  classes              => {
+    'puppet_enterprise::profile::master' => {
+      'r10k_remote'                 => 'https://github.com/TraGicCode/control-repo.git',
+      'code_manager_auto_configure' => true,
     },
-    environment          => 'production',
-    override_environment => false,
-    parent               => 'PE Master',
-    provider             => 'https',
-  }
-}
+    'pe_repo::platform::windows_x86_64' => {
 
-include puppetmaster
+    },
+  },
+  environment          => 'production',
+  override_environment => false,
+  parent               => 'PE Master',
+  provider             => 'https',
+}
