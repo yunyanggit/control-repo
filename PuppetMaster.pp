d7c859141aefb3f1552d75e1c99bf59d54e86ae9
tragiccode <michael@tragiccode.com>
2017-05-17 12:33:26 -0500

    Updated the ability to setup codemanager

--- PuppetMaster.pp
+++ PuppetMaster.pp
@@ -1,11 +1,12 @@
 class puppetmaster {
 
-  node_group { 'PuppetMaster':
-    ensure               => present,
+  node_group { 'PE Master - Customized':
+    ensure               => 'present',
     rule                 => ['and', ['~', ['fact', 'fqdn'], $facts['fqdn']]],
     classes              => {
-      'pe_r10k' => {
-        'remote' => 'https://github.com/TraGicCode/control-repo.git',
+      'puppet_enterprise::profile::master' => {
+        'r10k_remote'                 => 'https://github.com/TraGicCode/control-repo.git',
+        'code_manager_auto_configure' => true,
       },
       'pe_repo::platform::windows_x86_64' => {
 
@@ -13,7 +14,7 @@ class puppetmaster {
     },
     environment          => 'production',
     override_environment => false,
-    parent               => 'All Nodes',
+    parent               => 'PE Master',
     provider             => 'https',
   }
 }
