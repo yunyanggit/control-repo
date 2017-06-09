a741ce61f88553b2e047373934772657f15c9cd6
tragiccode <michael@tragiccode.com>
2017-06-09 13:28:37 -0500

    Move license file over

--- site/profile/manifests/puppetenterprise/puppetserver.pp
+++ site/profile/manifests/puppetenterprise/puppetserver.pp
@@ -2,7 +2,7 @@ class profile::puppetenterprise::puppetserver(
     String $file_source_pe_license = 'puppet:///modules/profile/puppetenterprise/puppetserver/license.key'
 ) {
 
-   file{ '/etc/puppetlabs/pe-license.key':
+   file{ '/etc/puppetlabs/license.key':
     ensure => present,
     source => $source,
     notify => Service['pe-console-services'],
