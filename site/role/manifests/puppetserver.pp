7e09ff197b37dc6a6acb452a26f5e647dab3e2b3
tragiccode <michael@tragiccode.com>
2017-06-09 13:48:45 -0500

    go

--- site/role/manifests/puppetserver.pp
+++ site/role/manifests/puppetserver.pp
@@ -1,7 +1,7 @@
 class role::puppetserver {
 
-  #This role would be made of all the profiles that need to be included to make a webserver work
-  #All roles should include the base profile
+  # This role would be made of all the profiles that need to be included to configure/manage our puppet enterprise server
+  # All roles should include the base profile
   include profile::base
   include profile::puppetenterprise::puppetserver
   
