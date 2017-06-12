f6e4308e2f2f8690b012e2b97b5b1c59d0731a56
tragiccode <michael@tragiccode.com>
2017-06-12 09:28:35 -0500

    WIP

--- spec/classes/puppetserver_spec.rb
+++ spec/classes/puppetserver_spec.rb
@@ -2,13 +2,21 @@ require 'spec_helper'
 
 describe 'profile::puppetenterprise::puppetserver' do
   context 'with default values for all parameters' do
-    file_source_pe_license = 'puppet:///modules/profile/puppetenterprise/puppetserver/license.key'
+    file_source_pe_license     = 'puppet:///modules/profile/puppetenterprise/puppetserver/license.key'
+    file_source_auto_sign_conf = 'puppet:///modules/profile/puppetenterprise/puppetserver/autosign.conf'
+
     let(:params) {{
       :file_source_pe_license => file_source_pe_license,
     }}
     it { should contain_file('/etc/puppetlabs/license.key').with({
         :ensure => 'present',
         :source => file_source_pe_license,
+        :notify => 'Service[pe-console-services]',
+    }) }
+
+    it { should contain_file('/etc/puppetlabs/puppet/autosign.conf').with({
+        :ensure => 'present',
+        :source => file_source_auto_sign_conf,
         :notify => 'Service[pe-console-services]',
     }) }
   end
