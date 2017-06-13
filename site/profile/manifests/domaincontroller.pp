4aea27415cca1b28936245ff07f65bcede205e33
tragiccode <michael@tragiccode.com>
2017-06-13 17:42:19 -0500

    Added more parameters to the DomainController profile

--- site/profile/manifests/domaincontroller.pp
+++ site/profile/manifests/domaincontroller.pp
@@ -2,7 +2,11 @@
 #
 #
 class profile::domaincontroller(
+  String $domain_name,
+  String[15] $domain_net_bios_name, 
+  String $safe_mode_administrator_user,
   String $safe_mode_administrator_password,
+  String $domain_administrator_user,
   String $domain_administrator_password,
 ) {
 
@@ -11,20 +15,20 @@ class profile::domaincontroller(
     dsc_name   => 'AD-Domain-Services',
   }
 
-  dsc_xaddomain { 'tragiccode.local':
+  dsc_xaddomain { $domain_name:
     ensure                            => 'present',
-    dsc_domainname                    => 'tragiccode.local',
+    dsc_domainname                    => $domain_name,
     dsc_safemodeadministratorpassword => {
-      'user'     => 'Administrator',
+      'user'     => $safe_mode_administrator_user,
       'password' => $safe_mode_administrator_password,
     },
     # Username & password to be assigned to our domain administrator account if this is our first domain in a forest.
     # else it's needed to join a new domain to an existing forest
     dsc_domainadministratorcredential => {
-      'user'     => 'Administrator2',
+      'user'     => $domain_administrator_user,
       'password' => $domain_administrator_password,
     },
-    dsc_domainnetbiosname             => 'TRAGICCODE', # 15 character limit...
+    dsc_domainnetbiosname             => $domain_net_bios_name, # 15 character limit...
     dsc_databasepath                  => 'C:\\Windows\\NTDS',
     dsc_logpath                       => 'C:\\Windows\\NTDS',
     dsc_sysvolpath                    => 'C:\\Windows\\SYSVOL',
@@ -34,6 +38,6 @@ class profile::domaincontroller(
   reboot { 'dsc_reboot':
     message => 'DSC has requested a reboot',
     when    => 'pending',
-    require => Dsc_xaddomain['tragiccode.local'],
+    require => Dsc_xaddomain[$domain_name],
   }
 }
