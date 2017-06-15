81b8d38e9ab8651b0ad6cb031769da5122ed27e5
tragiccode <michael@tragiccode.com>
2017-06-15 06:36:45 -0500

    Trying to redact parameters

--- site/profile/manifests/domaincontroller.pp
+++ site/profile/manifests/domaincontroller.pp
@@ -13,9 +13,9 @@ class profile::domaincontroller(
   String $domain_name,
   String[1, 15] $domain_net_bios_name,
   String $safe_mode_administrator_user,
-  String $safe_mode_administrator_password,
+  Sensitive[String] $safe_mode_administrator_password,
   String $domain_administrator_user,
-  String $domain_administrator_password,
+  Sensitive[String] $domain_administrator_password,
 ) {
 
   dsc_windowsfeature { 'AD-Domain-Services':
@@ -28,13 +28,13 @@ class profile::domaincontroller(
     dsc_domainname                    => $domain_name,
     dsc_safemodeadministratorpassword => {
       'user'     => $safe_mode_administrator_user,
-      'password' => Sensitive($safe_mode_administrator_password),
+      'password' => $safe_mode_administrator_password,
     },
     # Username & password to be assigned to our domain administrator account if this is our first domain in a forest.
     # else it's needed to join a new domain to an existing forest
     dsc_domainadministratorcredential => {
       'user'     => $domain_administrator_user,
-      'password' => Sensitive($domain_administrator_password),
+      'password' => $domain_administrator_password,
     },
     dsc_domainnetbiosname             => $domain_net_bios_name, # 15 character limit...
     dsc_databasepath                  => 'C:\\Windows\\NTDS',
