607d44831e1dbe59493b2cb7cbb95aeb24ec6700
tragiccode <michael@tragiccode.com>
2017-06-15 06:56:27 -0500

    Going to try redact

--- Puppetfile
+++ Puppetfile
@@ -1,10 +1,11 @@
 forge 'http://forge.puppetlabs.com'
 
 # Modules discovered by generate-puppetfile
-mod 'puppetlabs/dsc',           '1.3.0'
-mod 'puppet/splunk',            '6.0.0'
-mod 'puppet/staging',           '2.2.0'
-mod 'puppetlabs/inifile',       '1.6.0'
-mod 'puppetlabs/powershell',    '2.1.0'
-mod 'puppetlabs/stdlib',        '4.17.0'
-mod 'puppetlabs/reboot',        '1.2.1'
+mod 'binford2k/node_encrypt', '0.2.5'
+mod 'puppet/splunk', '6.0.0'
+mod 'puppet/staging', '2.2.0'
+mod 'puppetlabs/dsc', '1.3.0'
+mod 'puppetlabs/inifile', '1.6.0'
+mod 'puppetlabs/powershell', '2.1.0'
+mod 'puppetlabs/reboot', '1.2.1'
+mod 'puppetlabs/stdlib', '4.17.0'
--- spec/fixtures/modules/profile/manifests/domaincontroller.pp
+++ spec/fixtures/modules/profile/manifests/domaincontroller.pp
@@ -13,11 +13,14 @@ class profile::domaincontroller(
   String $domain_name,
   String[1, 15] $domain_net_bios_name,
   String $safe_mode_administrator_user,
-  Sensitive[String] $safe_mode_administrator_password,
+  String $safe_mode_administrator_password,
   String $domain_administrator_user,
-  Sensitive[String] $domain_administrator_password,
+  String $domain_administrator_password,
 ) {
-
+  
+  redact('safe_mode_administrator_password')
+  redact('domain_administrator_password')
+  
   dsc_windowsfeature { 'AD-Domain-Services':
     dsc_ensure => 'present',
     dsc_name   => 'AD-Domain-Services',
@@ -28,13 +31,13 @@ class profile::domaincontroller(
     dsc_domainname                    => $domain_name,
     dsc_safemodeadministratorpassword => {
       'user'     => $safe_mode_administrator_user,
-      'password' => $safe_mode_administrator_password,
+      'password' => Sensitive($safe_mode_administrator_password),
     },
     # Username & password to be assigned to our domain administrator account if this is our first domain in a forest.
     # else it's needed to join a new domain to an existing forest
     dsc_domainadministratorcredential => {
       'user'     => $domain_administrator_user,
-      'password' => $domain_administrator_password,
+      'password' => Sensitive($domain_administrator_password),
     },
     dsc_domainnetbiosname             => $domain_net_bios_name, # 15 character limit...
     dsc_databasepath                  => 'C:\\Windows\\NTDS',
