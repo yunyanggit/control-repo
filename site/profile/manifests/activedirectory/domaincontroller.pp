03518def0f4686e3430525b818c8697a818e33fd
tragiccode <michael@tragiccode.com>
2017-06-17 00:16:09 -0500

    Rebooting

--- site/profile/manifests/activedirectory/domaincontroller.pp
+++ site/profile/manifests/activedirectory/domaincontroller.pp
@@ -47,11 +47,11 @@ class profile::activedirectory::domaincontroller(
     dsc_logpath                       => 'C:\\Windows\\NTDS',
     dsc_sysvolpath                    => 'C:\\Windows\\SYSVOL',
     require                           => Dsc_windowsfeature['AD-Domain-Services'],
+    notify                            => Reboot['new_domain_controller_reboot'],
   }
 
   reboot { 'new_domain_controller_reboot':
     apply   => 'immediately',
-    when    => 'pending',
     message => 'New domain controller installed and is causing a reboot since one is pending',
     require => Dsc_xaddomain[$domain_name],
   }
