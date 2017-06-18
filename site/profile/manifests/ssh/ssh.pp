a9f865101d872794b607170bd1f7ce4d4c23760a
tragiccode <michael@tragiccode.com>
2017-06-18 17:48:01 -0500

    Ssh stuff

---
+++ site/profile/manifests/ssh/ssh.pp
@@ -0,0 +1,9 @@
+class profile::ssh::ssh(
+
+) {
+    # Prevent hacker from brute forcing accounts password
+    # Find sshd_config
+    # Set PasswordAUthentication to the following line
+    # PasswordAuthentication no
+    # Restart ssh service afterward to pick up new change
+}
