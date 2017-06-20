012c1c6c9ee5eb5bd14c5658a6b5a924342d91f6
tragiccode <michael@tragiccode.com>
2017-06-20 09:19:29 -0500

    Fix DN of ou

--- site/profile/manifests/activedirectory/organizationalunits.pp
+++ site/profile/manifests/activedirectory/organizationalunits.pp
@@ -8,7 +8,7 @@ class profile::activedirectory::organizationalunits(
   dsc_xadorganizationalunit { 'Domain Controllers':
     dsc_ensure      => 'present',
     dsc_name        => 'Domain Controllers',
-    dsc_path        => 'DC=gccthd,DC=com',
+    dsc_path        => 'DC=tragiccode,DC=local',
     dsc_description => 'Managed by Puppet! Changes made manually may be lost.',
   }
 }
