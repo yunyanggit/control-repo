f84b1c1175ccbaf4b547aed1fc6de5802c73cab8
tragiccode <michael@tragiccode.com>
2017-06-20 18:44:36 -0500

    Commented out base code for now.  needs tests

--- site/profile/manifests/base.pp
+++ site/profile/manifests/base.pp
@@ -1,6 +1,22 @@
 class profile::base {
 
   #the base profile should include component modules that will be on all nodes
-  include profile::firewall
-  include profile::defender
+  # include profile::firewall
+  # include profile::defender
+
+  # Lock users startup folder by changing permissions to prevent them from changing this
+  # this prevents hackers/maliciuous software from executing code on startup
+
+  # Lock the host file to prevent malicious software from redirecting the user to a fake website
+  # Maybe permissions is fine for this?
+
+  # Stop unnessary services we don't use to reduce attack surface
+  # Stop rdp
+  # stop smb
+  # Stop network places discovery
+
+  # start necessary services
+  # start antivirus if installed
+
+
 }
