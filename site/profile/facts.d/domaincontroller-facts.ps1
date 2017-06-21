9a8df69aefbbbf41b932864f5fed4a7805524931
tragiccode <michael@tragiccode.com>
2017-06-21 17:58:28 -0500

    Update domaincontroller facts

--- site/profile/facts.d/domaincontroller-facts.ps1
+++ site/profile/facts.d/domaincontroller-facts.ps1
@@ -27,21 +27,21 @@ if ($isDC -eq 'True') {
     Write-Output "$($factPrefix)is_global_catalog=$($thisDC.IsGlobalCatalog)"
     
     # Get the Forest functional level            
-    $thisForest = [System.DirectoryServices.ActiveDirectory.Forest]::GetCurrentForest()
+    $thisForest = Get-ADForest -Current LocalComputer
     Write-Output "$($factPrefix)forest_name=$($thisForest.Name)"
     Write-Output "$($factPrefix)forest_functional_level=$($thisForest.ForestMode.ToString().ToLower())"
 
-    Write-Output "$($factPrefix)is_fsmo_schema_role_owner=$($thisForest.SchemaRoleOwner.ToString().ToLower() -eq $thisDCName)"
-    Write-Output "$($factPrefix)is_fsmo_naming_role_owner=$($thisForest.NamingRoleOwner.ToString().ToLower() -eq $thisDCName)"
+    Write-Output "$($factPrefix)is_fsmo_schema_role_owner=$($thisForest.SchemaMaster.ToString().ToLower() -eq $thisDCName)"
+    Write-Output "$($factPrefix)is_fsmo_naming_role_owner=$($thisForest.DomainNamingMaster.ToString().ToLower() -eq $thisDCName)"
     
-    # Get the Forest functional level            
-    $thisDomain = [System.DirectoryServices.ActiveDirectory.Domain]::GetCurrentDomain()
-    $thisDomainName = $thisDomain.name
+    # Get the Domain functional level          
+    $thisDomain = Get-ADDomain -Current LocalComputer
+    $thisDomainName = $thisDomain.Name
     Write-Output "$($factPrefix)domain_functional_level=$($thisDomain.DomainMode.ToString().ToLower())"
 
-    Write-Output "$($factPrefix)is_fsmo_pdc_role_owner=$($thisDomain.PdcRoleOwner.ToString().ToLower() -eq $thisDCName)"
-    Write-Output "$($factPrefix)is_fsmo_rid_role_owner=$($thisDomain.RIDRoleOwner.ToString().ToLower() -eq $thisDCName)"
-    Write-Output "$($factPrefix)is_fsmo_infrastructure_role_owner=$($thisDomain.InfrastructureRoleOwner.ToString().ToLower() -eq $thisDCName)"
+    Write-Output "$($factPrefix)is_fsmo_pdc_role_owner=$($thisDomain.PDCEmulator.ToString().ToLower() -eq $thisDCName)"
+    Write-Output "$($factPrefix)is_fsmo_rid_role_owner=$($thisDomain.RIDMaster.ToString().ToLower() -eq $thisDCName)"
+    Write-Output "$($factPrefix)is_fsmo_infrastructure_role_owner=$($thisDomain.InfrastructureMaster.ToString().ToLower() -eq $thisDCName)"
   }
 }
 if ($thisDomainName -ne '') { Write-Output "$($factPrefix)domain_name=$($thisDomainName)" }
