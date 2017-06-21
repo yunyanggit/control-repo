c5b207d47613944be92e74337ac3e0e5249a5726
tragiccode <michael@tragiccode.com>
2017-06-21 03:42:31 -0500

    Fixing dependency

--- site/profile/manifests/activedirectory/domaincontroller.pp
+++ site/profile/manifests/activedirectory/domaincontroller.pp
@@ -108,6 +108,5 @@ class profile::activedirectory::domaincontroller(
   reboot { 'new_domain_controller_reboot':
     apply   => 'immediately',
     message => 'New domain controller installed and is causing a reboot since one is pending',
-    # require => Dsc_xaddomain[$domain_name],
   }
 }
