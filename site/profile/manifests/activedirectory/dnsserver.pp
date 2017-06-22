0ec9dbf1e4eb52fb6e9cceb6e625003942923c6c
tragiccode <michael@tragiccode.com>
2017-06-22 08:58:02 -0500

    fix name of var

--- site/profile/manifests/activedirectory/dnsserver.pp
+++ site/profile/manifests/activedirectory/dnsserver.pp
@@ -8,12 +8,12 @@ class profile::activedirectory::dnsserver {
     $matches = $network.match(/^(\d+)\.(\d+)\.(\d+)$/)
     # reverse the array to match reverse lookup zone expected format
     # turn array back into dotted notation
-    $st = reverse($matches.delete_at(0)).join('.')
+    $reversed_network = reverse($matches.delete_at(0)).join('.')
 
 
     dsc_xdnsserveradzone { 'Ipv4 Reverse Lookup Zone':
       dsc_ensure           => 'present',
-      dsc_name             => "${st}.in-addr.arpa",
+      dsc_name             => "${reversed_network}.in-addr.arpa",
       dsc_replicationscope => 'Forest',
       dsc_dynamicupdate    => 'None',
     }
