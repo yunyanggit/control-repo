eab6e5792cdf4569dba16480287aba26f4759e78
tragiccode <michael@tragiccode.com>
2017-06-26 07:46:58 -0500

    update

--- site/profile/manifests/linux/jenkinsmaster.pp
+++ site/profile/manifests/linux/jenkinsmaster.pp
@@ -18,6 +18,11 @@ class profile::linux::jenkinsmaster {
   jenkins::plugin { 'plain-credentials': }
   jenkins::plugin { 'script-security': }
   jenkins::plugin { 'scm-api': }
+  jenkins::plugin { 'workflow-support': }
+  jenkins::plugin { 'durable-task': }
+  jenkins::plugin { 'workflow-scm-step': }
+  jenkins::plugin { 'jquery-detached': }
+  jenkins::plugin { 'mailer': }
   # jenkins::plugin { 'workflow-aggregator': }
   # jenkins::plugin { 'git': }
   # jenkins::plugin { 'workflow-scm-step': }
