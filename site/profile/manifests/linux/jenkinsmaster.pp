d7b8781c045672a60c9940e33e0218673b78732d
tragiccode <michael@tragiccode.com>
2017-06-26 06:57:04 -0500

    Commenting out plugins for now

--- site/profile/manifests/linux/jenkinsmaster.pp
+++ site/profile/manifests/linux/jenkinsmaster.pp
@@ -4,26 +4,26 @@
 class profile::linux::jenkinsmaster {
   include jenkins
 
-  jenkins::plugin { 'workflow-scm-step': }
-  jenkins::plugin { 'workflow-step-api': }
-  jenkins::plugin { 'workflow-support': }
-  jenkins::plugin { 'workflow-cps': }
-  jenkins::plugin { 'workflow-durable-task-step': }
-  jenkins::plugin { 'workflow-api': }
-  jenkins::plugin { 'workflow-basic-steps': }
-  jenkins::plugin { 'ace-editor': }
-  jenkins::plugin { 'bouncycastle-api': }
-  jenkins::plugin { 'display-url-api': }
-  jenkins::plugin { 'durable-task': }
-  jenkins::plugin { 'jquery-detached': }
-  jenkins::plugin { 'mailer': }
-  jenkins::plugin { 'scm-api': }
-  jenkins::plugin { 'script-security': }
-  jenkins::plugin { 'plain-credentials': }
-  jenkins::plugin { 'structs': }
-  jenkins::plugin { 'copyartifact': }
-  jenkins::plugin { 'matrix-project': }
-  jenkins::plugin { 'git': }
-  jenkins::plugin { 'ssh-agent': }
-  jenkins::plugin { 'puppet-enterprise-pipeline': }
+  # jenkins::plugin { 'workflow-scm-step': }
+  # jenkins::plugin { 'workflow-step-api': }
+  # jenkins::plugin { 'workflow-support': }
+  # jenkins::plugin { 'workflow-cps': }
+  # jenkins::plugin { 'workflow-durable-task-step': }
+  # jenkins::plugin { 'workflow-api': }
+  # jenkins::plugin { 'workflow-basic-steps': }
+  # jenkins::plugin { 'ace-editor': }
+  # jenkins::plugin { 'bouncycastle-api': }
+  # jenkins::plugin { 'display-url-api': }
+  # jenkins::plugin { 'durable-task': }
+  # jenkins::plugin { 'jquery-detached': }
+  # jenkins::plugin { 'mailer': }
+  # jenkins::plugin { 'scm-api': }
+  # jenkins::plugin { 'script-security': }
+  # jenkins::plugin { 'plain-credentials': }
+  # jenkins::plugin { 'structs': }
+  # jenkins::plugin { 'copyartifact': }
+  # jenkins::plugin { 'matrix-project': }
+  # jenkins::plugin { 'git': }
+  # jenkins::plugin { 'ssh-agent': }
+  # jenkins::plugin { 'puppet-enterprise-pipeline': }
 }
