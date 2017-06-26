77b895f88b167e38f74f04fe4440142f1733b6ff
tragiccode <michael@tragiccode.com>
2017-06-26 07:58:54 -0500

    finally...

--- spec/fixtures/modules/profile/manifests/linux/jenkinsmaster.pp
+++ spec/fixtures/modules/profile/manifests/linux/jenkinsmaster.pp
@@ -4,10 +4,6 @@
 class profile::linux::jenkinsmaster {
   include jenkins
 
-  jenkins::user { 'tragiccode':
-    email    => 'michael@tragiccode.com',
-    password => 'changeme',
-  }
   jenkins::plugin { 'structs': }
   jenkins::plugin { 'puppet-enterprise-pipeline': }
   jenkins::plugin { 'workflow-api': }
@@ -25,28 +21,4 @@ class profile::linux::jenkinsmaster {
   jenkins::plugin { 'mailer': }
   jenkins::plugin { 'display-url-api': }
   jenkins::plugin { 'ace-editor': }
-  # jenkins::plugin { 'workflow-aggregator': }
-  # jenkins::plugin { 'git': }
-  # jenkins::plugin { 'workflow-scm-step': }
-  # jenkins::plugin { 'workflow-step-api': }
-  # jenkins::plugin { 'workflow-support': }
-  # jenkins::plugin { 'workflow-cps': }
-  # jenkins::plugin { 'workflow-durable-task-step': }
-  # jenkins::plugin { 'workflow-api': } 
-  # jenkins::plugin { 'workflow-basic-steps': }
-  # jenkins::plugin { 'ace-editor': }
-  # jenkins::plugin { 'bouncycastle-api': }
-  # jenkins::plugin { 'display-url-api': }
-  # jenkins::plugin { 'durable-task': }
-  # jenkins::plugin { 'jquery-detached': }
-  # jenkins::plugin { 'mailer': }   
-  # jenkins::plugin { 'scm-api': }
-  # jenkins::plugin { 'script-security': }  
-  # jenkins::plugin { 'plain-credentials': }
-  # jenkins::plugin { 'structs': }
-  # jenkins::plugin { 'copyartifact': }
-  # jenkins::plugin { 'matrix-project': }
-  # jenkins::plugin { 'git': }
-  # jenkins::plugin { 'ssh-agent': }
-  # jenkins::plugin { 'puppet-enterprise-pipeline': }
 }
