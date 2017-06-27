06a452fd88e9420cf933230550cb7861707e0a37
tragiccode <michael@tragiccode.com>
2017-06-27 14:23:36 -0500

    asd

--- site/profile/manifests/linux/jenkinsmaster.pp
+++ site/profile/manifests/linux/jenkinsmaster.pp
@@ -14,51 +14,45 @@ class profile::linux::jenkinsmaster {
   }
 
 
-  jenkins::plugin { 'structs': }
-  jenkins::plugin { 'puppet-enterprise-pipeline': }
-  jenkins::plugin { 'workflow-api': }
+ jenkins::plugin { 'puppet-enterprise-pipeline': }
+  jenkins::plugin { 'gitlab-plugin': }
+  jenkins::plugin { 'git': }
   jenkins::plugin { 'workflow-step-api': }
-  jenkins::plugin { 'workflow-basic-steps': }
-  jenkins::plugin { 'workflow-cps': }
-  jenkins::plugin { 'workflow-durable-task-step': }
+  jenkins::plugin { 'git-client': }
   jenkins::plugin { 'plain-credentials': }
+  jenkins::plugin { 'structs': }
   jenkins::plugin { 'script-security': }
-  jenkins::plugin { 'scm-api': }
-  jenkins::plugin { 'workflow-support': }
-  jenkins::plugin { 'durable-task': }
+  jenkins::plugin { 'workflow-cps': }
+  jenkins::plugin { 'workflow-api': }
+  jenkins::plugin { 'workflow-durable-task-step': }
   jenkins::plugin { 'workflow-scm-step': }
+  jenkins::plugin { 'ssh-credentials': }
+  jenkins::plugin { 'durable-task': }
+  jenkins::plugin { 'workflow-basic-steps': }
+  jenkins::plugin { 'workflow-support': }
+  jenkins::plugin { 'scm-api': }
+  jenkins::plugin { 'ace-editor': }
   jenkins::plugin { 'jquery-detached': }
   jenkins::plugin { 'mailer': }
+  jenkins::plugin { 'matrix-project': }
   jenkins::plugin { 'display-url-api': }
-  jenkins::plugin { 'ace-editor': }
+  jenkins::plugin { 'junit': }
+  jenkins::plugin { 'workflow-aggregator': }
   jenkins::plugin { 'pipeline-input-step': }
-  jenkins::plugin { 'workflow-job':
-    version => '2.12.1',
-  }
-  jenkins::plugin { 'workflow-cps-global-lib': }
-  jenkins::plugin { 'workflow-multibranch': }
-  jenkins::plugin { 'pipeline-stage-view': }
-  jenkins::plugin { 'pipeline-model-definition': }
-  jenkins::plugin { 'pipeline-stage-step': }
   jenkins::plugin { 'pipeline-milestone-step': }
+  jenkins::plugin { 'pipeline-build-step': }
+  jenkins::plugin { 'pipeline-stage-view': }
+  jenkins::plugin { 'workflow-multibranch': }
+  jenkins::plugin { 'pipeline-stage-step': }
+  jenkins::plugin { 'workflow-cps-global-lib': }
+  jenkins::plugin { 'workflow-job': }
+  jenkins::plugin { 'momentjs': }
   jenkins::plugin { 'pipeline-rest-api': }
+  jenkins::plugin { 'handlebars': }
   jenkins::plugin { 'cloudbees-folder': }
   jenkins::plugin { 'git-server': }
-  jenkins::plugin { 'pipeline-graph-analysis':  }
-  jenkins::plugin { 'branch-api':  }
-
-  # Git Plugin
-  jenkins::plugin { 'git': }
-  # Dependencies
-  jenkins::plugin { 'git-client': }
-  jenkins::plugin { 'junit': }
-  jenkins::plugin { 'matrix-project': }
-  jenkins::plugin { 'ssh-credentials': }
-
-  # SSH Plugin
-  jenkins::plugin { 'ssh-agent': }
-  # Dependencies
-  jenkins::plugin { 'bouncycastle-api': }
+  jenkins::plugin { 'branch-api': }
+  jenkins::plugin { 'pipeline-graph-analysis': }
   # jenkins::user { 'tragiccode':
   #   email    => 'michael@tragiccode.com',
   #   password => 'puppetlabs',
